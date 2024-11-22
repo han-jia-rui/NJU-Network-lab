af586f59c262ddb209c528257f998dfc13e5a34d
Jiarui Han <2536787472@qq.com>
2024-11-22 17:25:04 +0800

    bugfix(check3): Pass the additional test of TCPSender

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -11,9 +11,7 @@ uint64_t TCPSender::consecutive_retransmissions() const
   return consecutive_retransmissions_;
 }
 
-void TCPSender::transmit_wrapper( Segment& seg,
-                                  const TransmitFunction& transmit,
-                                  bool track )
+void TCPSender::transmit_wrapper( Segment& seg, const TransmitFunction& transmit, bool track )
 {
   transmit( TCPSenderMessage {
     .seqno { Wrap32::wrap( seg.seqno, isn_ ) },
@@ -43,14 +41,15 @@ void TCPSender::push( const TransmitFunction& transmit )
 
   Reader& reader = input_.reader();
   uint64_t seq_window = ack_base_ + ( window_size_ ? window_size_ : 1 );
+  if ( seq_window < seq_current_ )
+    return;
   uint64_t max_seq_size = seq_window - seq_current_;
 
   if ( seg.length() < max_seq_size )
     seg.SYN = ( seq_current_ == 0 );
 
   while ( reader.bytes_buffered() != 0 && max_seq_size > 0 ) {
-    uint64_t max_data_size
-      = std::min( TCPConfig::MAX_PAYLOAD_SIZE, max_seq_size - seg.length() );
+    uint64_t max_data_size = std::min( TCPConfig::MAX_PAYLOAD_SIZE, max_seq_size - seg.length() );
     seg.data = reader.peek().substr( 0, max_data_size );
     reader.pop( seg.data.size() );
 
@@ -87,8 +86,6 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
   if ( msg.ackno.has_value() ) {
     uint64_t ack_no = msg.ackno.value().unwrap( isn_, ack_base_ );
     if ( ack_no > ack_base_ && ack_no <= seq_current_ ) {
-      ack_base_ = ack_no;
-
       if ( RTO_ratio_ != 1 ) {
         RTO_ratio_ = 1;
         consecutive_retransmissions_ = 0;
@@ -99,6 +96,7 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
         Segment& seg = buffer_.front();
         if ( seg.seqno + seg.length() > ack_no )
           break;
+        ack_base_ = seg.seqno + seg.length();
         buffer_.pop();
         timer.restart();
       }
